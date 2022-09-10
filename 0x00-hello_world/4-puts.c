5bd46040b877f6177cff5be022ed9c876ed115f8
Dullon A Jaftha <alfescoj21@gmail.com>
2022-09-10 03:15:02 -0700

    C programming

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 /*Print char to the screen and return null*/
-int main(void) 
+int main(void)
 {
   puts("\"Programming is like building a multilingual puzzle\n");
   return (0);
