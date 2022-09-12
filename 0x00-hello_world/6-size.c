94394d185827705dfc6962b8240bd00faeecfe27
Dullon A Jaftha <alfescoj21@gmail.com>
2022-09-12 13:32:57 -0700

    Coding

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/* 
+ *Getting the size of the different data types in c programming language
+ * The main function will return all there details
+ *
+ * */
+
+int main(void)
+{
+
+    char c;
+    int i;
+    float f;
+    long dl;
+    long long lli;
+
+    printf("Size of char: %ld byte(s)",sizeof(c));
+    printf("Size of an int: %ld byte(s)", sizeof(i)); 
+    printf("Size of a long int: %ld byte(s)", sizeof(dl));
+    printf("Size of a long long int: %ld byte(s)", sizeof(lli));
+    print("Size of a float: %ld byte(s)", sizeof(f));
+
+	    
+    return (0);
+}	
+
