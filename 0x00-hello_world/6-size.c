05c11a118cae39abafb6f1b7d0e57b8220c13d7c
Dullon A Jaftha <alfescoj21@gmail.com>
2022-09-12 13:50:23 -0700

    Sizes

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -9,11 +9,11 @@
 int main(void)
 {
 
-    char c;
-    int i;
-    float f;
-    long int dl;
-    long long lli;
+    char c = 'a';
+    int i = 5;
+    float f = 1.2;
+    long int dl = 1000;
+    long long lli = 0x023;
 
     printf("Size of char: %ld byte(s)\n", sizeof(c));
     printf("Size of an int: %ld byte(s)\n", sizeof(i)); 
