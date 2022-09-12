4fca6df5ed373f0d2ad60f62d96ea9bb871e3ed7
Dullon A Jaftha <alfescoj21@gmail.com>
2022-09-12 12:53:57 -0700

    Printing the message to the screen with printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
+/* A c program that prints to the screen */
 int main(void)
 {
-  printf("with proper grammar, but the outcome is a piece of art,\n")
-  return (0)
+  printf("with proper grammar, but the outcome is a piece of art,\n");
+  return (0);
 }
