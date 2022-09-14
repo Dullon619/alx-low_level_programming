675a267454aa048b5d62be1d1a55adf8926a8882
Dullon A Jaftha <alfescoj21@gmail.com>
2022-09-14 09:46:48 -0700

    Files added

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,7 +1,14 @@
-#include <stdio.h>
+#include <unistd.h>
+/* 
+ * A custom function what is user to return a value{string} to the screen */
+int _putchar(char c)
+{
+	return(write(1, &c,1));
+}
+
 
 int main(void)
 {
-	printf("_putchar");
+	_putchar("_putchar");
 	return (0);
 }
