fabe5ab73ad4b6ec8021fb866af0eafdad8f0a53
Dullon A Jaftha <alfescoj21@gmail.com>
2022-09-15 09:35:40 -0700

    Alphabet in lowercase

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,6 +1,10 @@
 #include "main.h"
 
-/* Print the alphabet in all lowercase letters */
+/* 
+ * Print the alphabet in all lowercase letters 
+ *
+ * return: 0
+ * */
 
 void print_alphabet(void)
 {
