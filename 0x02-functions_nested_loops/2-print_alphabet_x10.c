9f9e01be68ebf61cfcac2a51973a1911d4293157
Dullon A Jaftha <alfescoj21@gmail.com>
2022-09-15 09:45:57 -0700

    Alphabet in lowecase

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -6,9 +6,10 @@
  * */
 void print_alphabet_x10(void)
 {
-	for(int i = 0; i <= 9; i++)
+	char c, i;
+	for(i = 0; i <= 9; i++)
 	{
-		for(char c = 'a'; c <= 'z'; c++)
+		for(c = 'a'; c <= 'z'; c++)
 		{
 			_putchar(c);
 		}
